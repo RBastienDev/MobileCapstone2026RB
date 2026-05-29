0858568d174bdcb9d28cad90a4e74e3e21d315ab
Robert B <aliceinchainsistayaway@gmail.com>
2026-05-29 08:32:27 -0400

    Initial Commit

---
+++ MobileCapstone2026RB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileCapstone2026RB
+//
+//  Created by Robert Bastien on 2026-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileCapstone2026RB/MobileCapstone2026RBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileCapstone2026RBApp.swift
+//  MobileCapstone2026RB
+//
+//  Created by Robert Bastien on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct MobileCapstone2026RBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
